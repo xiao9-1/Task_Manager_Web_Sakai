014b282df9796514a1ba80ba75dd074faee34a37
Kirill <matveeffew@gmail.com>
2026-06-24 15:39:40 +0300

    login button using Enter

--- src/app/pages/auth/login.ts
+++ src/app/pages/auth/login.ts
@@ -43,22 +43,15 @@ import { ModeService } from '@/app/service/mode.service';
                             <span class="text-muted-color font-medium">Sign in to continue</span>
                         </div>
 
-                        <div>
-                            <label for="email1" class="block text-surface-900 dark:text-surface-0 text-xl font-medium mb-2">Email</label>
-                            <input pInputText id="email1" type="text" placeholder="Email address" class="w-full md:w-120 mb-8" [(ngModel)]="email" />
+                        <form (ngSubmit)="login()">
+                            <label for="email1" class="block text-xl font-medium mb-2">Email</label>
+                            <input pInputText id="email1" type="text" placeholder="Email address" class="w-full md:w-120 mb-8" [(ngModel)]="email" name="email" />
 
-                            <label for="password1" class="block text-surface-900 dark:text-surface-0 font-medium text-xl mb-2">Password</label>
-                            <p-password id="password1" [(ngModel)]="password" placeholder="Password" [toggleMask]="true" styleClass="mb-4" [fluid]="true" [feedback]="false"></p-password>
+                            <label for="password1" class="block text-xl font-medium mb-2">Password</label>
+                            <p-password id="password1" [(ngModel)]="password" name="password" placeholder="Password" [toggleMask]="true" styleClass="mb-4" [fluid]="true" [feedback]="false"> </p-password>
 
-                            <div class="flex items-center justify-between mt-2 mb-8 gap-8">
-                                <div class="flex items-center">
-                                    <p-checkbox [(ngModel)]="checked" id="rememberme1" binary class="mr-2"></p-checkbox>
-                                    <label for="rememberme1">Remember me</label>
-                                </div>
-                                <span class="font-medium no-underline ml-2 text-right cursor-pointer text-primary">Forgot password?(Not avaliable)</span>
-                            </div>
-                            <p-button label="Sign In" styleClass="w-full" (click)="login()"></p-button>
-                        </div>
+                            <p-button label="Sign In" styleClass="w-full" type="submit"> </p-button>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -71,8 +64,6 @@ export class Login {
     password: string = '';
 
     errorMessage = '';
-
-    checked: boolean = false;
 
     constructor(
         private authService: AuthService,
